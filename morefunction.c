7ae1babc26420ae13502f7b31ae73fc6a4d6d3a1
Vicky <63452@holbertonstudents.com>
2023-08-31 05:21:24 -0700

    Monty

--- morefunction.c
+++ morefunction.c
@@ -46,7 +46,7 @@ int push(stack_t **stack, unsigned int line_number)
 	new_node = malloc(sizeof(stack_t));
 	if (new_node == NULL)
 	{
-		fprintf(stderr, "Error: malloc failed");
+		fprintf(stderr, "Error: malloc failed\n");
 		exit(EXIT_FAILURE);
 	}
 	new_node->n = num;
@@ -111,12 +111,12 @@ int main(int argc, char **argv)
 
 	if (argc != 2)
 	{
-		fprintf(stderr, "USAGE: monty file");
+		fprintf(stderr, "USAGE: monty file\n");
 		exit(EXIT_FAILURE); }
 	file = fopen(argv[1], "r");
 	if (file == NULL)
 	{
-		fprintf(stderr, "Error: Cannot open file %s\n", argv[1]);
+		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
 		exit(EXIT_FAILURE); }
 	while (getline(&buffer, &buffsize, file) != -1)
 	{
