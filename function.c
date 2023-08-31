8549937b8b8dcd43a505f723cc77bcbe7d5051af
Vicky <63452@holbertonstudents.com>
2023-08-31 05:31:01 -0700

    Monty

--- function.c
+++ function.c
@@ -28,7 +28,7 @@ int pop(stack_t **stack, unsigned int line_number)
 		fprintf(stderr, "L%u: can't pop an empty stack\n", line_number);
 		exit(EXIT_FAILURE);
 	}
-	
+
 	stack_t *temp = *stack;
 	*stack = (*stack)->next;
 
