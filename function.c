f2a79d0bdb1d719056faadba597d25cdb7370688
Vicky <63452@holbertonstudents.com>
2023-08-31 05:28:29 -0700

    Monty

--- function.c
+++ function.c
@@ -24,7 +24,7 @@ void pint(stack_t **stack, unsigned int line_number)
 int pop(stack_t **stack, unsigned int line_number)
 {
 	if (*stack == NULL)
-		fprintf(stderr, "L%u: can't pop an empty stack\n", line_number);
+		 fprintf(stderr, "L%u: can't pop an empty stack\n", line_number);
 		exit(EXIT_FAILURE);
 	stack_t *temp = *stack;
 	*stack = (*stack)->next;
